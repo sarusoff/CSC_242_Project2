0cd41d0eb7f60429a520b322f0f95d31004cef22
bigjackieg <bigjackieg@gmail.com>
2017-03-05 22:41:58 -0500

    Update TestDPLL.java

--- CSC_242_Project2/src/main/TestDPLL.java
+++ CSC_242_Project2/src/main/TestDPLL.java
@@ -6,6 +6,8 @@ import pl.core.Symbol;
 import pl.examples.HornClausesKB;
 import pl.examples.ModusPonensKB;
 import pl.examples.WumpusWorldKB;
+import pl.examples.TruthTellersKB;
+import pl.examples.AdvancedTruthTellersKB;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,10 +22,63 @@ public class TestDPLL {
         failures = testWumpusWorld(test,failures);
         failures = testModusPonens(test,failures);
         failures = testHornClauses(test,failures);
+        failures = testTruthTellers1(test,failures);
+        failures = testTruthTellers2(test,failures);
 
         printFailures(failures);
     }
-
+    private static List<String> testTruthTeller1(ModelChecking test, List<String> failures){
+        TruthTellersKB truthteller = new TruthTellersKB();
+        //part A
+        
+        //infer Amy is false
+       Sentence amy = new Symbol("amy");
+       if(test.entails(truthteller,amy)){
+           failures.add("Test failed: interAmyFalse");
+       }
+       //Bob is false
+       truthteller= new TruthTellersKB();
+        Sentence bob = new Symobl("bob");
+        if(test.entails(truthteller,bob)){
+           failures.add("Test failed: interBobFalse");
+       }
+   //----------------------------------------------------------     
+     //Part B
+     
+     //infer Amy is True
+       truthteller = new TruthTellersKB();
+      Sentence amy2 = new Symbol("amy2");
+      if(!test.entails(truthteller,amy2)){
+           failures.add("Test failed: interAmy2True");
+       }
+       //infer Amy is False
+        truthteller = new TruthTellersKB();
+         amy2= new Symbol("amy2");                        
+      if(test.entails(truthteller,amy2)){
+           failures.add("Test failed: interAmy2False");
+       }                          
+           }  
+                                 
+    private static List<String> testTruthTellers2(ModelChecking test, List<String> failures){
+        //infer Amy False
+        AdvancedTruthTellersKB Advtruthtellers = new AdvancedTruthTellersKB();
+        Sentence amy = new Symbol("amy");
+        if(test.entails(Advtruthtellers,amy){
+            failures.add("Test failed: inferAmyFalse"); 
+    }
+           //infer not Gil
+           Advtruthtellers = new AdvancedTruthTellersKB();
+             Sentence gil = new Symbol("gil");
+        if(test.entails(Advtruthtellers,amy){
+            failures.add("Test failed: inferGilFalse"); 
+    }
+          //infer not Ida
+          Advtruthtellers = new AdvancedTruthTellersKB();
+        Sentence ida = new Symbol("ida");
+        if(test.entails(Advtruthtellers,ida){
+            failures.add("Test failed: inferIdaFalse"); 
+    }
+           }
     private static List<String> testHornClauses(DPLL test, List<String> failures) {
         // tests question a
         //inferMythicalFalse
